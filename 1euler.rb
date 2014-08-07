5728189c55222ddb39edf6f0c547365db24b7624
Krystian <krystian.kawa1@gmail.com>
2014-08-07 20:24:44 +0200

    corrected exercises

---
+++ 1euler.rb
@@ -0,0 +1,12 @@
+def multiples(value)	
+	number = 0
+	for i in 1..value
+		if i%3 == 0 || i%5 == 0 then
+			number += i
+		end
+	end
+	return number
+end
+
+puts "Score for numbers below 10 is " + multiples(9).to_s
+puts "Score for numbers bellow 1000 is " + multiples(999).to_s
